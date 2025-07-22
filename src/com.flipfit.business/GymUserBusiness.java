b549fc947346557c4c82a41f6ac2642858e6cebd
ihita-self <sinhaihita@gmail.com>
2025-07-22 16:23:52 +0530

    business skeleton

---
+++ src/com.flipfit.business/GymUserBusiness.java
@@ -0,0 +1,10 @@
+package com.flipfit.business;
+
+import com.flipfit.bean.GymUser;
+
+public Class GymUserBusiness {
+    public void createUser(GymUser user) {
+        System.out.println("user created ig");
+    }
+    public void 
+}
