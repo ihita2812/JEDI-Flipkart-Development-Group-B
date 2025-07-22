0455df3529bd7a4def22519c74719f089a388999
ritesh1603 <2021csb1120@iitrpr.ac.in>
2025-07-22 17:45:36 +0530

    registration business

---
+++ src/com.flipfit.business/RegistrationBusiness.java
@@ -0,0 +1,11 @@
+package com.flipfit.business;
+
+import com.flipfit.bean.Registration;
+
+public class RegistrationBusiness {
+    public boolean verifyUser(Registration details)
+    {
+        System.out.println("User Verified");
+        return true;
+    }
+}
