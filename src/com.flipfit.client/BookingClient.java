c62fde9739627175e5674a6d64a7705dd525cb22
ihita-self <sinhaihita@gmail.com>
2025-07-22 18:01:32 +0530

    booking client

---
+++ src/com.flipfit.client/BookingClient.java
@@ -0,0 +1,10 @@
+package com.flipkart.client;
+
+import com.flipkart.business.BookingBusiness;
+
+public class BookingClient {
+	public static void main(String[] args ) {
+		Booking booking = new Booking();
+		booking.deleteBooking();
+	}
+}
