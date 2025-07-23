f46abfe889ed26c6f23de575be634ffcd3a94a0e
Aryan Goenka <aryangoenka999@gmail.com>
2025-07-23 12:36:06 +0530

    GymOwnerClient updated

--- JEDI-Flipfit-Java-POS/com.flipfit.client/GymOwnerClient.java
+++ JEDI-Flipfit-Java-POS/com.flipfit.client/GymOwnerClient.java
@@ -1,7 +1,52 @@
 package com.flipfit.client;
 
+import java.util.Scanner;
+import com.flipfit.bean.*;
 import com.flipfit.business.*;
 
 public class GymOwnerClient {
-    
+    public static void ownerMenu() {
+        GymOwnerBusiness ownerBusiness = new GymOwnerBusiness();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome Gym Owner!");
+        System.out.println("Enter your choice");
+        System.out.println("\t1\tRegister Gym Center");
+        System.out.println("\t2\tView Gym Centers");
+        System.out.println("\t3\tAdd Slots and Capacity");
+        System.out.println("\t4\tView Slot");
+        System.out.println("\t5\tView Notifications");
+        System.out.println("\t6\tView Payment");
+        System.out.println("\t7\tLogout");
+
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (choice) {
+            case 1:
+                ownerBusiness.registerGymCenter("CultFit", "Kormangla");
+                break;
+            case 2:
+                ownerBusiness.viewGymCenters(new GymOwner());
+                break;
+            case 3:
+                ownerBusiness.addSlotsAndCapacity(new GymCenter(), 5, 20);
+                break;
+            case 4:
+                ownerBusiness.viewSlot(new GymCenter());
+                break;
+            case 5:
+                ownerBusiness.viewNotifications();
+                break;
+            case 6:
+                ownerBusiness.viewPayment(new GymCenter());
+                break;
+            case 7:
+                return;
+            default:
+                System.out.println("Invalid choice.");
+        }
+
+
+    }
 }
