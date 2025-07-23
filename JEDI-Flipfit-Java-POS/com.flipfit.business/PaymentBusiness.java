8fa44d0bbec85953f7e69084ff05d68b2655a03f
ihita-self <sinhaihita@gmail.com>
2025-07-23 11:12:44 +0530

    delete services

--- JEDI-Flipfit-Java-POS/com.flipfit.business/PaymentBusiness.java
+++
@@ -1,9 +0,0 @@
-package com.flipfit.business;
-
-import com.flipfit.bean.Payment;
-
-public class PaymentBusiness {
-    public void makePayment(Payment payment) {
-        System.out.println("Did some payment?");
-    }
-}
--- JEDI-Flipfit-Java-POS/com.flipfit.business/SlotBusiness.java
+++
@@ -1,9 +0,0 @@
-package com.flipfit.business;
-
-import com.flipfit.bean.Slot;
-
-public class SlotBusiness {
-    public boolean isAvail(Slot slot) {
-        return true;
-    }
-}
